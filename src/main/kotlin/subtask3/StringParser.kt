eaf839a209c4f90bbc0232cb807e3014c026b86c
vittal94 <evg994z@mail.ru>
2021-05-22 11:16:07 +0300

    feat: resolved 3th task

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,38 @@ package subtask3
 
 class StringParser {
 
-    // TODO: Complete the following function
+
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val result = mutableListOf<String>()
+
+        for (i in inputString.indices){
+            if (inputString[i] == '(' || inputString[i] == '[' || inputString[i] == '<') {
+                val index = i + 1
+                val element = inputString[i]
+                val flipElement = when (element) {
+                    '<' -> '>'
+                    '(' -> ')'
+                    else -> ']'
+                }
+                val exceptElement = when (element) {
+                    '<' -> charArrayOf('(','[')
+                    '[' -> charArrayOf('<','(')
+                    else -> charArrayOf('<','[')
+                }
+                var isExceptElement = false
+
+                for (j in index until inputString.length) {
+                    if (inputString[j] in exceptElement)
+                        isExceptElement = true
+
+                    if (inputString[j] == element && !isExceptElement)
+                        break
+
+                    if (inputString[j] == flipElement)
+                        result.add(inputString.substring(index,j))
+                }
+            }
+        }
+        return result.toTypedArray()
     }
 }
